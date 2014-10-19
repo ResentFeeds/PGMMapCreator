8744ab87f4b675d6d0110b5418b9e7968af0d8a5
SkylerTyler1337 <tylernichols409@gmail.com>
2014-10-18 23:10:19 -0500

    Here is the main Class

---
+++ me/skylertyler1337/pgmmapcreator/MapCreatorPlugin.java
@@ -0,0 +1,35 @@
+package me.skylertyler.pgmmapcreator;
+
+//== Not Sure if this works 100% (to be avised that this is still in development...)
+import me.skylertyler.pgmmapcreator.listener.ConnectionListener;
+import me.skylertyler.pgmmapcreator.utils.Utils;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MapCreatorPlugin extends JavaPlugin {
+
+	public PluginManager pm;
+
+	public Utils utils = new Utils(this);
+	@Override
+	public void onEnable() {
+	}
+	
+	public void registerListeners(){
+		Utils u = this.getUtils();
+		u.registerListener(new ConnectionListener(this));
+	}
+	
+	public PluginManager getPluginManager() {
+		return this.pm;
+	}
+	
+	
+	public Utils getUtils(){
+		return this.utils;
+	}
+	@Override
+	public void onDisable() {
+	}
+}
